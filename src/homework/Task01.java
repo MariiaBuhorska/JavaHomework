8377055bc1c2e9e20621c6714cc0f31896306ade
m.bugorskaya@gmail.com <m.bugorskaya@gmail.com>
2022-08-27 12:51:58 +0300

    Homework 27.08.2022

---
+++ src/homework/Task01.java
@@ -0,0 +1,14 @@
+package homework;
+public class Task01 {
+    //В переменных х и y хранятся два натуральных числа. Создайте программу, выводящую на консоль:
+    //■ результат целочисленного деления x на y;
+    //■ остаток от деления x на y;
+    //■ квадратный корень x
+    public static void main(String[] args) {
+    int x = 121;
+    int y = 45;
+        System.out.println("Результат целочисленного деления x на y = " + x / y);
+        System.out.println("Остаток от деления x на y = " + x % y);
+        System.out.println("Квадратный корень x = " + Math.sqrt(x));
+    }
+}
---
+++ src/homework/Task06.java
@@ -0,0 +1,6 @@
+package homework;
+public class Task06 {
+    public static void main(String[] args) {
+
+    }
+}
